cf4ce193b61f8b46e95ba18c06075181ffa906d9
Phillip Taylor <phillip.taylor@autotrader.co.uk>
2016-03-17 15:21:21 +0000

    Setup to use ThymeLeaf to render the pages, with example of usage.

--- src/main/java/web/Application.java
+++ src/main/java/web/Application.java
@@ -10,15 +10,7 @@ import java.util.Arrays;
 public class Application {
 
     public static void main(String[] args) {
-        final ApplicationContext context = SpringApplication.run(Application.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = context.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/web/controller/HomeController.java
+++ src/main/java/web/controller/HomeController.java
@@ -1,14 +1,21 @@
 package web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @RequestMapping(path="/", method = RequestMethod.GET)
+    public ModelAndView index(@RequestParam(name = "name", required = false) String name) {
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name", name);
+        modelAndView.setViewName("index");
+        return modelAndView;
     }
 
 }
